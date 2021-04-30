e0eeb37054d586490e45ab703857cc6c3a477295
João Queiroz <joaoqueiroz-code@gmail.com>
2021-04-30 12:39:03 -0300

    Add visibilidade ao player após dar play

--- js/main.js
+++ js/main.js
@@ -4,26 +4,43 @@ var music1 = document.getElementById("music1");
 var music2 = document.getElementById("music2");
 var music3 = document.getElementById("music3");
 
+var musics = document.getElementsByClassName(".titulo");
+var playIcon = document.querySelector(".playIcon");
+var pauseIcon = document.querySelector(".pauseIcon")
+
+
+
+function mudarIcon() {
+    playIcon.innerHTML = "&#10074&#10074;"
+    console.log("agora ta no pause")
+    
+}
+
 
 function mudarAudio () {
 
-    music1.addEventListener("click",  function (){audio.src = "sounds/hit-the-road-jack.mp3"})
-    music2.addEventListener("click",  function (){audio.src = "sounds/thats-life.mp3"})
-    music3.addEventListener("click",  function (){audio.src = "sounds/fortunate-son.mp3"})
+   
+    music1.addEventListener("click",  function (){
+        
+        audio.src = "sounds/hit-the-road-jack.mp3"
+        audio.style.visibility = "visible";
+        mudarIcon();
+
+    })
+
+    music2.addEventListener("click",  function (){
+        audio.src = "sounds/thats-life.mp3"
+        audio.style.visibility = "visible";
+    })
+
+    music3.addEventListener("click",  function (){
+        audio.src = "sounds/fortunate-son.mp3"
+        audio.style.visibility = "visible";
+    })
 
     
 }
 
-function controleDoAudio () {
 
-    function pausado() {
-        audio.pause();
-
-    }
-
-    music1.addEventListener("click", pausado())
-    
-}
 
 mudarAudio();
-pausarAudio();
